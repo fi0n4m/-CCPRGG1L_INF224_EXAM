ebeac10dac927f9884512b487dffd551ea39d5f5
fi0n4m <111779860+fi0n4m@users.noreply.github.com>
2022-09-06 10:13:17 +0800

    Create App.java

---
+++ exam_1/App.java
@@ -0,0 +1,22 @@
+public class App { 
+    public static void main(String[] args) { 
+
+
+    // Character Class
+    Student Fiona = new Student();
+
+    Fiona.surname = "Martinez";
+    Fiona.firstName = "Fiona";
+    Fiona.middleInitial = 'B';
+    Fiona.dateOfBirth = "02/14/2004";
+    Fiona.studentNumber = 2022104386;
+    Fiona.studentEmailAddress = "martinezfb@students.national-u.edu.ph";
+    Fiona.iAmAwesome = true; 
+
+    Fiona.sayMyStudentNumber();
+    Fiona.sayMyEmailAddress();
+    Fiona.AmIAwesome();
+    }
+} 
+
+     
